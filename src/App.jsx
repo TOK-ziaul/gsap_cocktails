fa7fd40f0fdae7a204c48f4f3bffdec00950cd9e
Ziaul Khan <ziaul@teamofkeys.com>
2025-11-15 23:11:49 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap";
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return <div className="flex-center h-[100vh]">xdvdsv </div>;
+};
+
+export default App;
